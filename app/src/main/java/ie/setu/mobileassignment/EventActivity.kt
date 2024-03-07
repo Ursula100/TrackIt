e0cd85bcca31a01f97a8494e68bfba36c6aa6071
tamen <20100902@mail.wit.ie>
2024-03-07 14:51:50 +0000

    add SetOnClickListener for addBtn

--- app/src/main/java/ie/setu/mobileassignment/EventActivity.kt
+++ app/src/main/java/ie/setu/mobileassignment/EventActivity.kt
@@ -2,16 +2,25 @@ package ie.setu.mobileassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.setu.mobileassignment.databinding.ActivityEventBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class EventActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEventBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event)
+
+        binding = ActivityEventBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
         i("Trackit Activity started..")
+
+        binding.addBtn.setOnClickListener() {
+            i("add Button Pressed")
+        }
 
     }
 }
