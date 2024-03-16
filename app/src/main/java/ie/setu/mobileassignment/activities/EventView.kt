0f5c988fa8e740a57c92666b2a3c12b9c2fa1272
tamen <20100902@mail.wit.ie>
2024-03-16 16:15:08 +0000

    Create view_event.xml which holds individual events. Add RecyclerView to display all events. RycyclerView uses EventAdapter class.

--- app/src/main/java/ie/setu/mobileassignment/activities/EventView.kt
+++ app/src/main/java/ie/setu/mobileassignment/activities/EventView.kt
@@ -2,15 +2,56 @@ package ie.setu.mobileassignment.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ie.setu.mobileassignment.R
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ie.setu.mobileassignment.databinding.ActivityEventViewBinding
+import ie.setu.mobileassignment.databinding.ViewEventBinding
 import ie.setu.mobileassignment.main.MainApp
+import ie.setu.mobileassignment.models.EventModel
 
 class EventView : AppCompatActivity() {
 
+    private lateinit var binding: ActivityEventViewBinding
     lateinit var app: MainApp
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event_view)
+        binding = ActivityEventViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = EventAdapter(app.events)
+    }
+}
+
+class EventAdapter constructor(private var events: List<EventModel>) :
+    RecyclerView.Adapter<EventAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = ViewEventBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val event = events[holder.adapterPosition]
+        holder.bind(event)
+    }
+
+    override fun getItemCount(): Int = events.size
+
+    class MainHolder(private val binding : ViewEventBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(event: EventModel) {
+            binding.viewEventTitle.text = event.title
+            binding.viewEventDescription.text = event.description
+            "${event.startTime} - ${event.endTime}".also { binding.viewEventTime.text = it }
+        }
     }
 }
--- app/src/main/java/ie/setu/mobileassignment/main/MainApp.kt
+++ app/src/main/java/ie/setu/mobileassignment/main/MainApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import ie.setu.mobileassignment.models.EventModel
 import timber.log.Timber
 import timber.log.Timber.i
+import java.time.LocalDate
 
 class MainApp : Application() {
 
@@ -13,6 +14,13 @@ class MainApp : Application() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         i("App started")
+
+        events.add(EventModel("test 1", "", "Waterford", LocalDate.now(), LocalDate.now(), "9:10 AM", "20: 15 PM"))
+        events.add(EventModel("test 2", "Whatever1", "", LocalDate.now(), LocalDate.now(), "", "20:15 PM"))
+        events.add(EventModel("test 3", "Whatever2", "London", LocalDate.now(), LocalDate.now(), "9:10", ""))
+        events.add(EventModel("test 1", "", "Waterford", LocalDate.now(), LocalDate.now(), "9:10 AM", "20: 15 PM"))
+        events.add(EventModel("test 2", "Whatever1", "", LocalDate.now(), LocalDate.now(), "", "20:15 PM"))
+        events.add(EventModel("test 3", "Whatever2", "London", LocalDate.now(), LocalDate.now(), "9:10", ""))
     }
 
 }
